8e5de8ce2e8b4fb886ec63555396aa74c33e9979
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-29 17:53:02 +0200

    first commit

---
+++ ColectiiDeDate/ColectiiDeDate/IntArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColectiiDeDate
+{
+    class IntArray
+    {
+	public IntArray()
+        {
+            // construiește noul șir
+        }
+
+        public void Add(int element)
+        {
+            // adaugă un nou element la sfârșitul șirului 
+        }
+
+        public int Count()
+        {
+            // întorce numărul de elemente din șir
+            return 1;
+        }
+
+        public int Element(int index)
+        {
+            // întoarce elementul de la indexul dat
+            return 1;
+        }
+
+        public void SetElement(int index, int element)
+        {
+            // modifică valoarea elementului de la indexul dat
+        }
+
+        public bool Contains(int element)
+        {
+            // întoarce true dacă elementul dat există în șir
+            return true;
+        }
+
+        public int IndexOf(int element)
+        {
+            // întoarce indexul elementului sau -1 dacă elementul nu se
+            // regăsește în șir
+            return 1;
+        }
+
+        public void Insert(int index, int element)
+        {
+            // adaugă un nou element pe poziția dată
+        }
+
+        public void Clear()
+        {
+            // șterge toate elementele din șir
+        }
+
+        public void Remove(int element)
+        {
+            // șterge prima apariție a elementului din șir	
+        }
+
+        public void RemoveAt(int index)
+        {
+            // șterge elementul de pe poziția dată
+        }
+    }
+}
