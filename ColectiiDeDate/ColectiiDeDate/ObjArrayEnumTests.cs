69bc3d80f71e2340488f3090607ebbc3171da567
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-02-18 12:50:03 +0200

    more tests

--- ColectiiDeDate/ColectiiDeDate/ObjArrayEnumTests.cs
+++ ColectiiDeDate/ColectiiDeDate/ObjArrayEnumTests.cs
@@ -23,5 +23,31 @@ namespace ColectiiDeDate
 
             Assert.Equal(4, testArray.Current);
         }
+
+        [Fact]
+        public void EnumerateAnArrayWithTwoElementsReturnFirstElement()
+        {
+            var array = new ObjectArray { 2, 4 };
+
+            var testArray = array.GetEnumerator();
+            testArray.MoveNext();
+
+            Assert.Equal(2, testArray.Current);
+        }
+
+        [Fact]
+        public void EnumerateAnArrayWithTwoElementsAplyResetMethodRetunrNumberOfElements()
+        {
+            var array = new ObjectArray { 2, 4 };
+
+            var testArray = array.GetEnumerator();
+            testArray.MoveNext();
+            testArray.MoveNext();
+
+            testArray.Reset();
+            testArray.MoveNext();
+
+            Assert.Equal(2, testArray.Current);
+        }
     }
 }
